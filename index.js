d177fc6141fc2419095db0a6f57efb6ca1cbe9bf
Jazmin Matos <jmatos1993@gmail.com>
2022-08-03 12:52:07 -0400

    Added character movement

--- index.js
+++ index.js
@@ -10,9 +10,47 @@ map.src = './images/sprouts_farm_map.png'
 const playerImage = new Image();
 playerImage.src = './images/bunnyDown.png'
 
-map.onload = () => {
+class Sprite {
+    constructor({position,velocity, image}) {
+        this.position = position
+        this.velocity = velocity
+        this.image = image
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.position.x, this.position.y) // tile size 48px by 48px
+    }
+}
+
+const background = new Sprite({
+    position: {
+        x: -232, 
+        y: -480
+    },
+    image: map
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+const animate = () => {
+    window.requestAnimationFrame(animate)
+    console.log(animate)
+
     ctx.imageSmoothingQuality = 'high'
-    ctx.drawImage(map, -256, -528) // 48px difference....528
+    background.draw()
     ctx.drawImage(
         playerImage, 
         // cropping
@@ -21,26 +59,57 @@ map.onload = () => {
         playerImage.width / 4, // crop width
         playerImage.height, //crop height
         // actual width & height of image
-        canvas.width / 2 - playerImage.width / 4, // x coordinate
-        canvas.height / 2 + playerImage.height, // y coordinate
+        canvas.width / 2 - (playerImage.width / 4) / 2, // x coordinate
+        canvas.height / 2 - playerImage.height, // y coordinate
         playerImage.width/4,
         playerImage.height
     )
+
+    // TODO: Decide whether player should be able to move diagonally
+    if (keys.w.pressed) background.position.y += .75
+    if (keys.a.pressed) background.position.x += .75
+    if (keys.s.pressed) background.position.y -= .75
+    if (keys.d.pressed) background.position.x -= .75
 }
+
+animate()
 
 const onKeyDown = (e) => {
     switch (e.key) {
         case 'w':
-            console.log('pressed w  key')
+            keys.w.pressed = true
+            // console.log(`pressed ${e.key}`)
             break
         case 'a':
-            console.log('pressed a  key')
+            keys.a.pressed = true
+            // console.log(`pressed ${e.key}`)
             break
         case 's':
-            console.log('pressed s  key')
+            keys.s.pressed = true
+            // console.log(`pressed ${e.key}`)
             break
         case 'd':
-            console.log('pressed d  key')
+            keys.d.pressed = true
+            // console.log(`pressed ${e.key}`)
+            break
+        default:
+            break
+    }
+}
+
+const onKeyUp = (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
             break
         default:
             break
@@ -48,8 +117,4 @@ const onKeyDown = (e) => {
 }
 
 window.addEventListener('keydown', onKeyDown);
-
-
-const animate = () => {
-
-}
+window.addEventListener('keyup', onKeyUp);
